7380ce9f47d467f0140f44f75aead97d2b782447
Sain Rai <sahinkulung@gmail.com>
2026-08-14 00:22:53 +0545

    update navbar styles and portfolio images

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './navbar.css'
+import logo from '../assets/logo.png';  // adjust based on what the dir command shows
+import contact from '../assets/contact.png';
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className='navbar'>
+        <img src={logo} alt='logo' className='logo'/>
+        <div className='desktopMenu'>
+            <Link className='desktopMenuListItem'>Home</Link>
+            <Link className='desktopMenuListItem'>Clients</Link>
+            <Link className='desktopMenuListItem'>About</Link>
+            <Link className='desktopMenuListItem'>Portfolio</Link>
+
+        </div>
+        <button className='desktopMenuBtn'>
+            <img src={contact} alt='contact' className='desktopMenuImg'/>
+            Contact ME</button>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar; 
